8d9a6de9773686bcf3f07cf6dd068cfba3f86396
Nico <nicobargioni@gmail.com>
2025-08-28 16:37:06 -0300

    PP

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ st.set_page_config(
 st.title("📊 Nomadic | Dashboard de Medios")
 st.markdown("---")
 
-st.markdown("### 🏠 Página Principal")
-st.info("📋 Los dashboards están disponibles a través de URLs específicas proporcionadas por el administrador.")
+st.markdown("### Esta página es para nosotros. NO VA A EXISTIR EN PRODUCCIÓN.")
+st.info("📋 Los dashboards están disponibles a través de URLs que nosotros le vamos a dar al cliente.")
+st.info("👈 En el sidebar de la izquierda están todas las páginas. ESTO NO LO VA A VER EL CLIENTE NI LA REDACCIÓN, ES PARA COMODIDAD DURANTE LA ETAPA DE PRE PROUDCCION.")
 st.markdown("---")
-st.markdown("*Para acceder a un dashboard específico, utiliza la URL proporcionada.*")
+st.markdown("*Cada cuenta tiene 2 paneles: REDACCIÓN Y CLIENTE.*")
